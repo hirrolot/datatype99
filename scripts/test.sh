9a6744322e0036001d9bd7fbb9fed1fa4b9adf5e
hirrolot <hirrolot@gmail.com>
2021-02-03 15:19:38 +0600

    More tests

---
+++ scripts/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc tests.c -I. -Iepilepsy/include -ftrack-macro-expansion=0 -Wall -Wextra -pedantic -std=c99
--- tests.c
+++ tests.c
@@ -1,5 +1,9 @@
 #include <datatype99.h>
 
-datatype(Test1, (A), (B, int), (C, int, const char *, double));
+datatype(Test1, (Test1A), (Test1B, int), (Test1C, int, const char *, double));
+
+datatype(Test2, (Test2A));
+datatype(Test3, (Test3A));
+datatype(Test4, (Test4A), (Test4B), (Test4C));
 
 int main(void) {}
