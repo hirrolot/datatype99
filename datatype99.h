b496eac70e087f83581d0a1075a2882ad34cae20
hirrolot <hirrolot@gmail.com>
2021-02-03 15:16:00 +0600

    Implement empty variants

--- datatype99.h
+++ datatype99.h
@@ -1,8 +1,6 @@
 #ifndef DATATYPE99_H
 #define DATATYPE99_H
 
-#include <datatype99/unit_t.h>
-
 #include <stddef.h>
 
 #include <epilepsy.h>
@@ -28,6 +26,7 @@
         enum { EPILEPSY_eval(DATATYPE99_PRIV_GEN_TAGS(v(name), v(variants))) } tag;                \
                                                                                                    \
         union {                                                                                    \
+            char dummy;                                                                            \
             EPILEPSY_eval(DATATYPE99_PRIV_GEN_UNION_FIELDS(v(name), v(variants)))                  \
         } data;                                                                                    \
     } name;                                                                                        \
@@ -77,16 +76,19 @@
 #define DATATYPE99_PRIV_GEN_TYPEDEFS_IMPL(name, variants)                                          \
     EPILEPSY_listUnwrap(DATATYPE99_PRIV_MAP_VARIANTS(                                              \
         EPILEPSY_callTrivial(EPILEPSY_appl, DATATYPE99_PRIV_GEN_TYPEDEFS_MAP, name),               \
-        EPILEPSY_listFilter(                                                                       \
-            EPILEPSY_callTrivial(                                                                  \
-                EPILEPSY_compose,                                                                  \
-                EPILEPSY_not,                                                                      \
-                DATATYPE99_PRIV_IS_EMPTY_VARIANT),                                                 \
-            v(variants))))
+        v(variants)))
 
-#define DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_IMPL(name, tag, ...)                                      \
+#define DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_IMPL(name, ...)                                           \
     EPILEPSY_call(                                                                                 \
-        DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_AUX,                                                      \
+        EPILEPSY_ifPlain(                                                                          \
+            DATATYPE99_PRIV_IS_EMPTY_VARIANT(__VA_ARGS__),                                         \
+            EPILEPSY_consume,                                                                      \
+            DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_AUX),                                                 \
+        v(name, __VA_ARGS__))
+
+#define DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_AUX_IMPL(name, tag, ...)                                  \
+    EPILEPSY_call(                                                                                 \
+        DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_AUX_AUX,                                                  \
         v(name, tag) EPILEPSY_listUnwrap(EPILEPSY_listMapI(                                        \
             v(DATATYPE99_PRIV_GEN_VARIANT_FIELD),                                                  \
             EPILEPSY_callTrivial(EPILEPSY_list, __VA_ARGS__)))                                     \
@@ -94,7 +96,7 @@
                 EPILEPSY_callTrivial(EPILEPSY_appl, DATATYPE99_PRIV_GEN_FIELD_TYPEDEF, tag),       \
                 EPILEPSY_callTrivial(EPILEPSY_list, __VA_ARGS__))))
 
-#define DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_AUX_IMPL(name, tag, fields, field_typedefs)               \
+#define DATATYPE99_PRIV_GEN_TYPEDEFS_MAP_AUX_AUX_IMPL(name, tag, fields, field_typedefs)           \
     v(typedef struct {fields} name##tag; field_typedefs typedef struct name tag##ChoiceT;)
 
 #define DATATYPE99_PRIV_GEN_VARIANT_FIELD_IMPL(x, i)      v(x _##i;)
@@ -116,7 +118,13 @@
         EPILEPSY_callTrivial(EPILEPSY_appl, DATATYPE99_PRIV_GEN_UNION_FIELDS_MAP, name),           \
         v(variants)))
 
-#define DATATYPE99_PRIV_GEN_UNION_FIELDS_MAP_IMPL(name, tag, ...) v(name##tag tag;)
+#define DATATYPE99_PRIV_GEN_UNION_FIELDS_MAP_IMPL(name, ...)                                       \
+    v(EPILEPSY_ifPlain(                                                                            \
+        DATATYPE99_PRIV_IS_EMPTY_VARIANT(__VA_ARGS__),                                             \
+        EPILEPSY_consumePlain,                                                                     \
+        DATATYPE99_PRIV_GEN_UNION_FIELD)(name, __VA_ARGS__))
+
+#define DATATYPE99_PRIV_GEN_UNION_FIELD(name, tag, ...) name##tag tag;
 // }
 
 // Generate value constructors {
@@ -125,7 +133,16 @@
         EPILEPSY_callTrivial(EPILEPSY_appl, DATATYPE99_PRIV_GEN_CONSTRUCTORS_MAP, name),           \
         v(variants)))
 
-#define DATATYPE99_PRIV_GEN_CONSTRUCTORS_MAP_IMPL(name, tag, ...)                                  \
+#define DATATYPE99_PRIV_GEN_CONSTRUCTORS_MAP_IMPL(name, ...)                                       \
+    EPILEPSY_ifPlain(                                                                              \
+        DATATYPE99_PRIV_IS_EMPTY_VARIANT(__VA_ARGS__),                                             \
+        DATATYPE99_PRIV_GEN_CONSTRUCTOR_1,                                                         \
+        DATATYPE99_PRIV_GEN_CONSTRUCTOR_2)(name, __VA_ARGS__)
+
+#define DATATYPE99_PRIV_GEN_CONSTRUCTOR_1(name, tag_)                                              \
+    v(inline static name tag_(void) { return ((name){.tag = tag_##Tag, .data.dummy = '\0'}); })
+
+#define DATATYPE99_PRIV_GEN_CONSTRUCTOR_2(name, tag, ...)                                          \
     EPILEPSY_call(                                                                                 \
         DATATYPE99_PRIV_GEN_CONSTRUCTORS_MAP_AUX,                                                  \
         v(name, tag) EPILEPSY_callTrivial(EPILEPSY_list, __VA_ARGS__))
@@ -147,10 +164,8 @@
 #define DATATYPE99_PRIV_GEN_PARAM_NAME_IMPL(_x, i) v(_##i)
 // }
 
-#define DATATYPE99_PRIV_IS_EMPTY_VARIANT_IMPL(variant)                                             \
-    EPILEPSY_uintEq(                                                                               \
-        EPILEPSY_variadicsCount(EPILEPSY_callTrivial(EPILEPSY_unparenthesise, variant)),           \
-        v(1))
+#define DATATYPE99_PRIV_IS_EMPTY_VARIANT(...)                                                      \
+    EPILEPSY_uintEqPlain(EPILEPSY_variadicsCountPlain(__VA_ARGS__), 1)
 
 #define DATATYPE99_PRIV_MAP_VARIANTS_IMPL(f, variants)                                             \
     EPILEPSY_listMap(                                                                              \
@@ -168,8 +183,6 @@
 #define DATATYPE99_PRIV_GEN_FIELD_TYPEDEF_ARITY    3
 #define DATATYPE99_PRIV_GEN_PARAM_ARITY            2
 #define DATATYPE99_PRIV_GEN_PARAM_NAME_ARITY       2
-
-#define DATATYPE99_PRIV_IS_EMPTY_VARIANT_ARITY 1
 // }
 
 #endif // DATATYPE99_H
---
+++ tests.c
@@ -0,0 +1,5 @@
+#include <datatype99.h>
+
+datatype(Test1, (A), (B, int), (C, int, const char *, double));
+
+int main(void) {}
