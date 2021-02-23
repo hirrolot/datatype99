cf7666ff500831aae445d183e78f8235cc955f1e
hirrolot <hirrolot@gmail.com>
2021-02-23 10:48:34 +0600

    Temporarily remove Result<T, E>
    Delayed until the next release.

--- datatype99.h
+++ datatype99.h
@@ -39,23 +39,11 @@
 #define Unit Unit99
 #define unit unit99
 
-#define Result      Result99
-#define isResultOk  isResultOk99
-#define isResultErr isResultErr99
-#define tryResult   tryResult99
-
 #endif // DATATYPE99_NO_ALIASES
 
 // Unit type {
 typedef char Unit99;
 static const Unit99 unit99 = '\0';
-// }
-
-// Result<T, E> {
-#define Result99(name, T, E) datatype99(name##Result, (name##Ok, T), (name##Err, E))
-
-#define isResultOk99(result)  ((int)(result).tag == 0)
-#define isResultErr99(result) ((int)(result).tag == 1)
 // }
 
 // Parsing {
