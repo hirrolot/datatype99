e99659e778aa0ff4e5a009911c27c075e0eb9df5
hirrolot <hirrolot@gmail.com>
2021-04-23 11:02:31 +0600

    Fix examples/derive/metadata.c

--- examples/derive/metadata.c
+++ examples/derive/metadata.c
@@ -21,7 +21,7 @@
         ML99_braced(                                                                               \
             ML99_assign(v(.name), v(#tag)),                                                        \
             ML99_assign(v(.arity), ML99_listLen(v(sig))),                                          \
-            ML99_assign(v(.size), v(sizeof(name_##Tag)))),                                         \
+            ML99_assign(v(.size), v(sizeof(name_##tag)))),                                         \
         v(, ))
 #define GEN_VARIANT_ARITY 2
 
@@ -74,9 +74,11 @@ static const DatatypeMetadata Num_metadata = {
 int main(void) {
 
 #define CHECK(idx, name_, arity_, size_)                                                           \
-    assert(strcmp(Num_metadata.variants[idx].name, name_) == 0);                                   \
-    assert(arity_ == Num_metadata.variants[idx].arity);                                            \
-    assert(size_ == Num_metadata.variants[idx].size)
+    do {                                                                                           \
+        assert(strcmp(Num_metadata.variants[idx].name, name_) == 0);                               \
+        assert(arity_ == Num_metadata.variants[idx].arity);                                        \
+        assert(size_ == Num_metadata.variants[idx].size);                                          \
+    } while (0)
 
     CHECK(0, "Char", 1, sizeof(char));
     CHECK(1, "Int", 1, sizeof(int));
