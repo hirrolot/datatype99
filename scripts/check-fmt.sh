6b979ea5d2198c3e5c9640881a7a224c13fb8dcc
hirrolot <hirrolot@gmail.com>
2021-12-01 20:53:40 +0600

    Fix indentation in `scripts/check-fmt.sh`

--- scripts/check-fmt.sh
+++ scripts/check-fmt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./run-clang-format/run-clang-format.py --exclude examples/build --exclude tests/build -r datatype99.h tests examples
+./run-clang-format/run-clang-format.py \
+    --exclude examples/build \
+    --exclude tests/build \
+    -r datatype99.h tests examples
